96db84be09b7c81aa27a5722375b319942f8dfdf
Bizjuks <maksims.bizjuks@sk.lvt.lv>
2024-04-30 09:00:02 +0300

    Glaba

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,15 +6,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleGroup;
 
 
 
 public class Main extends Application  {
 	@Override
 	public void start (Stage primaryStage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FX-scen.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main_scene.fxml"));
 		Scene scene = new Scene(fxmlLoader.load());
+//		ToggleGroup pizzaGroup = (ToggleGroup) scene.lookup("#$pizza");
+		
+//		 pizzaGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+//	            if (newValue != null) {
+//	                System.out.println("Selected RadioButton: " + newValue.getUserData());
+//	            }
+//	        });
 
+	      
+	
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module Bizjuks_Pica2 {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
